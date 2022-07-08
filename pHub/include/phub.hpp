2648bc5136224539a8d46a78aca7f30522b5fd0a
Esteve Soria <esofabian@gmail.com>
2022-07-08 22:55:24 +0200

    Wij assign hubs :yes:

--- pHub/include/phub.hpp
+++ pHub/include/phub.hpp
@@ -5,18 +5,22 @@
 #ifndef P_HUB_GENETIC_ALGORITHM_PHUB_HPP
 #define P_HUB_GENETIC_ALGORITHM_PHUB_HPP
 
+#include <utility>
 #include <vector>
 #include <map>
+#include <optional>
 #include "Individual.hpp"
-#include "dataset.hpp"
 
 class pHub {
 public:
+    explicit pHub(std::vector<std::vector<double>> dataset) : data_(std::move(dataset)),
+                                                              n_nodes_(sqrt(dataset.size())) {}
+
     double calculate_fitness(const Individual &individual) {
         auto accumulator{0.0f};
-        for (auto i = 0; i < nodes; ++i) {
-            for (auto j = 0; j < nodes; ++j) {
-                if(i==j) {
+        for (auto i = 0; i < n_nodes_; ++i) {
+            for (auto j = 0; j < n_nodes_; ++j) {
+                if (i == j) {
                     continue;
                 }
 
@@ -25,29 +29,46 @@ public:
         return 0;
     }
 
-    void assign_hubs(const Individual &individual){
-        std::map<size_t, std::vector<size_t>> assigned;
-        for(auto i=0;i < individual.chromosomes_.size(); i++) {
-            if(individual.chromosomes_.get_allele(i)){
-                std::vector<std::vector<double>> nodes_connected(map.begin() + i*nodes, map.begin() + i*nodes + 25);
+    std::map<size_t, size_t> assign_hubs(const Individual &individual) {
+        std::map<size_t, size_t> assigned;
+        std::vector<size_t> hubs_indexes;
+        for (auto i = 0; i < individual.chromosomes_.size(); i++) {
+            if (individual.chromosomes_.get_allele(i)) {
+                hubs_indexes.emplace_back(i);
             }
         }
-
+        for (auto node_idx = 0; node_idx < n_nodes_; ++node_idx) {
+            auto lowest_cost = std::numeric_limits<double>::max();
+            auto lowest_hub_idx = std::optional<size_t>();
+            if (std::find(hubs_indexes.begin(), hubs_indexes.end(), node_idx) != hubs_indexes.end()) {
+                continue;
+            }
+            for (auto h_idx: hubs_indexes) {
+                auto cost = c(node_idx, h_idx);
+                if (cost < lowest_cost) {
+                    lowest_cost = cost;
+                    lowest_hub_idx = h_idx;
+                }
+            }
+            assigned.emplace(node_idx, *lowest_hub_idx);
+        }
+        return assigned;
     }
 
-    static double w(size_t start, size_t end) {
+    double w(size_t start, size_t end) {
         return get_row(start, end)[2];
     }
 
-    static double c(size_t start, size_t end) {
+    double c(size_t start, size_t end) {
         return get_row(start, end)[3];
     }
 
 private:
-    static constexpr auto nodes = 25;
+    const size_t n_nodes_;
+    std::vector<std::vector<double>> data_{};
 
-    static const std::vector<double> &get_row(size_t start, size_t end) {
-        return map[(start - 1) * nodes + (end - 1)];
+    const std::vector<double> &get_row(size_t start, size_t end) {
+        return data_[(start * n_nodes_) + end];
     }
 
 };
--- pHub/tests/phub_tests.cpp
+++ pHub/tests/phub_tests.cpp
@@ -5,14 +5,42 @@
 #include "catch2/catch.hpp"
 
 #include "phub.hpp"
-#include "dataset.hpp"
+#include <dataset.hpp>
 
 TEST_CASE("Getting values from dataset") {
-    pHub problem;
-    REQUIRE(problem.w(1,1)==map[0][2]);
-    REQUIRE(problem.c(1,1)==map[0][3]);
+    const std::vector<std::vector<double>> map = {{0, 0, 0, 0},
+                                                  {0, 1, 6469, 576.9631},
+                                                  {0, 2, 7629, 946.4954},
+                                                  {1, 0, 6469, 576.9631},
+                                                  {1, 1, 0   , 0.0},
+                                                  {1, 2, 7629, 800.0},
+                                                  {2, 0, 7629, 946.4954},
+                                                  {2, 1, 7629, 800.0},
+                                                  {2, 2, 0   , 0.0}};
+    pHub problem(map);
+    REQUIRE(problem.w(0, 1) == map[1][2]);
+    REQUIRE(problem.c(0, 1) == map[1][3]);
 
-    REQUIRE(problem.w(25,1)==map[600][2]);
-    REQUIRE(problem.c(25,1)==map[600][3]);
+    REQUIRE(problem.w(1, 1) == map[26][2]);
+    REQUIRE(problem.c(1, 1) == map[26][3]);
+}
+
+TEST_CASE("Hub assign test") {
+    const std::vector<std::vector<double>> mock_data = {{0, 0, 0, 0},
+                                                  {0, 1, 6469, 576.9631},
+                                                  {0, 2, 7629, 946.4954},
+                                                  {1, 0, 6469, 576.9631},
+                                                  {1, 1, 0   , 0.0},
+                                                  {1, 2, 7629, 800.0},
+                                                  {2, 0, 7629, 946.4954},
+                                                  {2, 1, 7629, 800.0},
+                                                  {2, 2, 0   , 0.0}};
+    Individual i(25);
+    i.chromosomes_.print();
+    pHub problem(map);
+    auto connections = problem.assign_hubs(i);
+    for(auto c : connections){
+        std::cout << c.first << ' ' << c.second << std::endl;
+    }
 
 }
