f3c4e50aae87fddb669785a9c0deb7785ae5f41f
Esteve Soria <esofabian@gmail.com>
2022-07-09 22:41:20 +0200

    Use arguments in main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@
 #include "pHub/include/dataset.hpp"
 
 int main(int argc, char *argv[]) {
-    pHub problem(phub_data, 1);
-    ssGA algorithm(problem, 5, 25, 0.6, 0.9);
+    // args: n_iter individuals gnome_length prob_cross prob_muta
+    for(auto i=1;i<argc; ++i) {
+        std::cout << argv[i] << " ";
+    }
+    std::cout << std::endl;
 
-    size_t n_iterations{100000};
+    pHub problem(phub_data, 1);
+    ssGA algorithm(problem, atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]));
+
+    size_t n_iterations{static_cast<size_t>(atoi(argv[1]))};
 
     for(auto i=0; i < n_iterations; ++i){
         algorithm.fit_population();
